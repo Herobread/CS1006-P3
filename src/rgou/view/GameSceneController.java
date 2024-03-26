86563494a3a6a8a90c684bdc6afd54e6ee661805
Alex Nazarenko <alexeybread81@gmail.com>
2024-03-26 12:02:47 +0000

    remove unused imports

--- src/rgou/view/GameSceneController.java
+++ src/rgou/view/GameSceneController.java
@@ -6,8 +6,6 @@ import java.util.Map;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import rgou.view.scenes.GameOverScene;
 import rgou.view.scenes.GameSceneBase;
--- src/rgou/view/MainFrame.java
+++ src/rgou/view/MainFrame.java
@@ -1,17 +1,10 @@
 package rgou.view;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 import java.awt.Dimension;
 import java.awt.Toolkit;
-import java.awt.BorderLayout;
-
-import rgou.view.scenes.Scenes;
 
 public class MainFrame extends JFrame {
-	private GameSceneController gameSceneController;
-	private JPanel activeScenePanel;
-
 	public MainFrame() {
 		setTitle("Royal game of Ur");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
