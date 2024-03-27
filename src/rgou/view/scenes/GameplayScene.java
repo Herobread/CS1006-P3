7258266507ae9bfe64155bbce338211b2d404509
Alex Nazarenko <alexeybread81@gmail.com>
2024-03-27 09:51:44 +0000

    refactor gameplay scene

--- src/rgou/view/scenes/GameplayScene.java
+++ src/rgou/view/scenes/GameplayScene.java
@@ -1,7 +1,6 @@
 package rgou.view.scenes;
 
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -10,6 +9,7 @@ import javax.swing.BorderFactory;
 import rgou.view.GameSceneController;
 import rgou.view.GameScenes;
 import rgou.view.components.ImageButton;
+import rgou.view.components.RenderContext;
 import rgou.view.sceneTemplates.GameSceneBase;
 
 public class GameplayScene extends GameSceneBase {
@@ -18,13 +18,10 @@ public class GameplayScene extends GameSceneBase {
 	}
 
 	public void run() {
-		double scale = gameSceneController.getSceneScale();
-		System.out.println(scale);
-		System.out.println("rerender");
+		RenderContext renderContext = new RenderContext(gameSceneController.getSceneScale());
 
-		ImageButton goBackButton = new ImageButton("assets/textures/pawns/pawn-black.png");
-		goBackButton.setBounds((int) (248 * scale), (int) (226 * scale), (int) (40 * scale), (int) (40 * scale));
-
+		ImageButton goBackButton = new ImageButton("pawns/pawn-black.png");
+		goBackButton.setBounds(renderContext.scaleRectangle(248, 226, 40, 40));
 		goBackButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -35,8 +32,6 @@ public class GameplayScene extends GameSceneBase {
 		add(goBackButton);
 
 		setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));
-
-		setPreferredSize(new Dimension((int) (300 * scale), 100));
 
 		repaint();
 	}
